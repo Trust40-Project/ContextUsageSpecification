bb0c13abd7a382bc4c150aec72a76ce8166cf593
thomas <uywqj@kit.edu>
2019-12-28 14:48:57 +0100

    Refactor structure to add tests

---
+++ Tests/src/uihandler/ButtonHandler.java
@@ -0,0 +1,32 @@
+package uihandler;
+
+
+import java.util.Objects;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+import util.MyLogger;
+import util.Util;
+
+public class ButtonHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+        Objects.requireNonNull(event);
+        
+        MyLogger.info("Tests-Start");
+
+    	//TODO do properly, currently use active workspace, only 1 project allowed
+        final String currentPath = Util.getCurrentDir();        
+        MyLogger.info(currentPath);
+        
+        //Call Mainhandler with datapath
+        //new MainHandler().execute(currentPath);
+
+        MyLogger.info("Tests-End");
+        
+        return null;
+	}
+}
